3d132c1ded84277276270f54f3e97a0faf290871
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2022-10-29 16:32:33 +0200

    Fixed legacy ticks and deprecated expr per Codacy

--- entry_script.sh
+++ entry_script.sh
@@ -17,20 +17,20 @@
 
 if [[ "$EXPORT_PERIOD" != 'daily' ]]; then
     MODE=HOURLY
-    FROM=$((`date -d "$(date +%Y/%m/%d' '%H:00:00 -d '1 hour ago' -u)" -u +%s` * 1000))
-    TO=$((`date -d "$(date +%Y/%m/%d' '%H:59:59 -d '1 hour ago' -u)" -u +%s` * 1000))
+    FROM=$(($(date -d "$(date +%Y/%m/%d' '%H:00:00 -d '1 hour ago' -u)" -u +%s) * 1000))
+    TO=$(($(date -d "$(date +%Y/%m/%d' '%H:59:59 -d '1 hour ago' -u)" -u +%s) * 1000))
 else
     MODE=DAILY
-    FROM=$((`date -d 'yesterday 00:00:00' -u +%s` * 1000))
-    TO=$((`date -d 'yesterday 23:59:59' -u +%s` * 1000))
+    FROM=$(($(date -d 'yesterday 00:00:00' -u +%s) * 1000))
+    TO=$(($(date -d 'yesterday 23:59:59' -u +%s) * 1000))
 fi
 
-FROM_TS=$(date -d @`expr $FROM / 1000` +%F'T'%T'Z' -u)
-TO_TS=$(date -d @`expr $TO / 1000` +%F'T'%T'Z' -u)
+FROM_TS=$(date -d @$(($FROM / 1000)) +%F'T'%T'Z' -u)
+TO_TS=$(date -d @$(($TO / 1000)) +%F'T'%T'Z' -u)
 
 echo -e "Creating [$MODE][$FROM_TS to $TO_TS] export task request..."
 
-RESPONSE=$(aws logs create-export-task --task-name log-group-$((`date -u +%s` * 1000)) \
+RESPONSE=$(aws logs create-export-task --task-name log-group-$(($(date -u +%s) * 1000)) \
     --log-group-name "$LOG_GROUP_NAME" \
     --from "$FROM" \
     --to "$TO" \
